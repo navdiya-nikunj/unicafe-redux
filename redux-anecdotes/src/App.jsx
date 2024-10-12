f44d899efde66a90b0d928b25d91975c75548b4d
navdiya-nikunj <nikunjmnavdiya@gmail.com>
2024-10-12 12:02:02 +0530

    Exercise 6.14

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,9 +1,19 @@
+import { useDispatch } from 'react-redux';
 import AnecDoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
 import Notification from './components/Notification';
+import { useEffect } from 'react';
+import notes from './services/notes';
+import { setAnecdote } from './reducers/anecdoteReducer';
 
 const App = () => {
+	const dispatch = useDispatch();
+	useEffect(() => {
+		notes.getAll().then((res) => {
+			dispatch(setAnecdote(res));
+		});
+	}, []);
 	return (
 		<div>
 			<Notification />
